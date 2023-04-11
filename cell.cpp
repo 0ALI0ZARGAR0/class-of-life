6ad9b0cff3dbefdb741a5b04dd90e0728dff00d2
ali_z <ali3.zargar16@gmail.com>
2023-04-11 19:14:59 +0330

    debugged cell

--- cell.cpp
+++ cell.cpp
@@ -44,20 +44,13 @@ public:
     void smallMutation(string A, string C, int n, int m);
     void greatMutation(string S1, int n, string S2, int m);
     void reverseMutation(string S1, int n);
-    void palindrome();
+    void palindrome(int n);
     void death();
+    void print(int n) { cout << "Your chromosome" << n + 1 << "'s DNA is: " << chromosome[n].DNA[0] << " | " << chromosome[n].DNA[1] << endl; }
 };
 
 int main()
 {
-    genome testgene = genome("TTACG", "TTACG", "AATGC");
-    cell testcell = cell("CCAGC", "GGTCG");
-    // testgene.createDNA();
-    // testgene.reverseMutation("TTA");
-    // testgene.smallMutation("T", "G", 5);
-    testgene.greatMutation("T", "CCCC");
-    testgene.print();
-
     return 0;
 }
 
@@ -182,44 +175,41 @@ void genome::smallMutation(string A, string C, int n)
 }
 void genome::greatMutation(string S1, string S2)
 {
-    int p01 = findSubstr(DNA[0], S1);
-    int p11 = findSubstr(DNA[1], S1);
-    int p02 = findSubstr(DNA[0], S2);
-    int p12 = findSubstr(DNA[1], S2);
-    if (p01 == -1 and p02 == -1)
+    int posInDNA0 = findSubstr(DNA[0], S1);
+    int posInDNA1 = findSubstr(DNA[1], S1);
+    if (posInDNA0 == -1 and posInDNA1 == -1)
     {
-        return;
+        cout << "This DNA doesn't have string \"" << S1 << "\"\n";
     }
-    else if ((p01 < p11 or p11 == -1) and p01 != -1)
+    else if ((posInDNA0 < posInDNA1 or posInDNA1 == -1) and posInDNA0 != -1)
     {
-        string temp = DNA[0].substr(p01, S1.length());
-        DNA[0] = replaceStr(DNA[0], p01, S1.length(), DNA[0], p11, S2.length());
-        DNA[0] = replaceStr(DNA[0], p11, S2.length(), temp);
-        temp = DNA[1].substr(p01, S1.length());
-        DNA[1] = replaceStr(DNA[1], p01, S1.length(), DNA[1], p11, S2.length());
-        DNA[1] = replaceStr(DNA[1], p11, S2.length(), temp);
+        DNA[0] = replaceStr(DNA[0], posInDNA0, S1.length(), S2);
+        DNA[1] = replaceStr(DNA[1], posInDNA0, S1.length(), complementary(S2));
     }
     else
     {
-        string temp = DNA[1].substr(p11, S1.length());
-        DNA[1] = replaceStr(DNA[1], p11, S1.length(), DNA[1], p12, S2.length());
-        DNA[1] = replaceStr(DNA[1], p12, S2.length(), temp);
-        temp = DNA[0].substr(p11, S1.length());
-        DNA[0] = replaceStr(DNA[0], p11, S1.length(), DNA[0], p12, S2.length());
-        DNA[0] = replaceStr(DNA[0], p12, S2.length(), temp);
+        DNA[1] = replaceStr(DNA[1], posInDNA1, S1.length(), S2);
+        DNA[0] = replaceStr(DNA[0], posInDNA1, S1.length(), complementary(S2));
     }
+    int posInRna = findSubstr(RNA, S1);
+    if (posInRna != -1)
+        RNA = replaceStr(RNA, posInRna, S1.length(), S2);
+    else
+        cout << "This RNA doesn't have string \"" << S1 << "\"\n";
 }
 void genome::reverseMutation(string S1)
 {
-    if (findSubstr(DNA[0], S1) < findSubstr(DNA[1], S1))
+    int pos0 = findSubstr(DNA[0], S1);
+    int pos1 = findSubstr(DNA[1], S1);
+    if ((pos0 < pos1 or pos1 == -1) and pos0 != -1)
     {
-        int p = findSubstr(DNA[0], S1);
+        int p = pos0;
         DNA[0] = reverseStr(DNA[0], p, p + S1.length());
         DNA[1] = reverseStr(DNA[1], p, p + S1.length());
     }
     else
     {
-        int p = findSubstr(DNA[1], S1);
+        int p = pos1;
         DNA[0] = reverseStr(DNA[0], p, p + S1.length());
         DNA[1] = reverseStr(DNA[1], p, p + S1.length());
     }
@@ -230,75 +220,120 @@ void genome::reverseMutation(string S1)
 // cell functions:
 void cell::smallMutation(string A, string C, int n, int m)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
     {
-        int pos0 = findSubstr(chromosome[m].DNA[0], A);
-        int pos1 = findSubstr(chromosome[m].DNA[1], A);
-        if (pos0 < pos1 or (pos1 == -1 and pos0 != -1))
+        int pos0 = findSubstr(chromosome[n].DNA[0], A);
+        int pos1 = findSubstr(chromosome[n].DNA[1], A);
+        if (pos1 == -1 and pos0 == -1)
         {
-            chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pos0, A.length(), C);
-            chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pos0, A.length(), complementary(C));
+            break;
+        }
+        else if ((pos0 < pos1 and pos0 != -1) or (pos1 == -1 and pos0 != -1))
+        {
+            chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pos0, A.length(), C);
+            chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pos0, A.length(), complementary(C));
         }
         else
         {
-            chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pos1, A.length(), C);
-            chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pos1, A.length(), complementary(C));
+            chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pos1, A.length(), complementary(C));
+            chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pos1, A.length(), C);
         }
     }
 }
 void cell::greatMutation(string S1, int n, string S2, int m)
 {
-    if (findSubstr(chromosome[n].DNA[0], S1) < findSubstr(chromosome[n].DNA[1], S1))
+    int pn0 = findSubstr(chromosome[n].DNA[0], S1);
+    int pn1 = findSubstr(chromosome[n].DNA[1], S1);
+    int pm0 = findSubstr(chromosome[m].DNA[0], S2);
+    int pm1 = findSubstr(chromosome[m].DNA[1], S2);
+    if ((pn0 == -1 and pn1 == -1))
     {
-        int p1 = findSubstr(chromosome[n].DNA[0], S1);
-        int p2 = findSubstr(chromosome[m].DNA[0], S2);
-        string temp = chromosome[n].DNA[0].substr(p1, S1.length());
-        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], p1, S1.length(), chromosome[m].DNA[0], p2, S2.length());
-        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], p2, S2.length(), temp);
-        temp = chromosome[n].DNA[1].substr(p1, S1.length());
-        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], p1, S1.length(), chromosome[m].DNA[1], p2, S2.length());
-        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], p2, S2.length(), temp);
+        cout << "DNA" << n << " doesn't have string \"" << S1 << "\"\n";
+        return;
     }
-    else
+    else if ((pm0 == -1 and pm1 == -1))
     {
-        int p1 = findSubstr(chromosome[n].DNA[1], S1);
-        int p2 = findSubstr(chromosome[m].DNA[1], S2);
-        string temp = chromosome[n].DNA[1].substr(p1, S1.length());
-        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], p1, S1.length(), chromosome[m].DNA[1], p2, S2.length());
-        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], p2, S2.length(), temp);
-        temp = chromosome[n].DNA[0].substr(p1, S1.length());
-        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], p1, S1.length(), chromosome[m].DNA[0], p2, S2.length());
-        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], p2, S2.length(), temp);
+        cout << "DNA" << m << " doesn't have string \"" << S2 << "\"\n";
+        return;
+    }
+    else if (((pn0 < pn1 or pn1 == -1) and pn0 != -1) and ((pm0 < pm1 or pm1 == -1) and pm0 != -1))
+    {
+        string temp = chromosome[n].DNA[0].substr(pn0, S1.length());
+        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pn0, S1.length(), chromosome[m].DNA[0], pm0, S2.length());
+        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pm0, S2.length(), temp);
+        temp = chromosome[n].DNA[1].substr(pn0, S1.length());
+        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pn0, S1.length(), chromosome[m].DNA[1], pm0, S2.length());
+        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pm0, S2.length(), temp);
+        return;
+    }
+    else if (!((pn0 < pn1 or pn1 == -1) and pn0 != -1) and ((pm0 < pm1 or pm1 == -1) and pm0 != -1))
+    {
+        string temp = chromosome[n].DNA[1].substr(pn1, S1.length());
+        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pn1, S1.length(), chromosome[m].DNA[0], pm0, S2.length());
+        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pm0, S2.length(), temp);
+        temp = chromosome[n].DNA[0].substr(pn1, S1.length());
+        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pn1, S1.length(), chromosome[m].DNA[1], pm0, S2.length());
+        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pm0, S2.length(), temp);
+        return;
+    }
+    else if (((pn0 < pn1 or pn1 == -1) and pn0 != -1) and !((pm0 < pm1 or pm1 == -1) and pm0 != -1))
+    {
+        string temp = chromosome[n].DNA[0].substr(pn0, S1.length());
+        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pn0, S1.length(), chromosome[m].DNA[1], pm1, S2.length());
+        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pm1, S2.length(), temp);
+        temp = chromosome[n].DNA[1].substr(pn0, S1.length());
+        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pn0, S1.length(), chromosome[m].DNA[0], pm1, S2.length());
+        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pm1, S2.length(), temp);
+        return;
+    }
+    else if (!((pn0 < pn1 or pn1 == -1) and pn0 != -1) and !((pm0 < pm1 or pm1 == -1) and pm0 != -1))
+    {
+        string temp = chromosome[n].DNA[1].substr(pn1, S1.length());
+        chromosome[n].DNA[1] = replaceStr(chromosome[n].DNA[1], pn1, S1.length(), chromosome[m].DNA[1], pm1, S2.length());
+        chromosome[m].DNA[1] = replaceStr(chromosome[m].DNA[1], pm1, S2.length(), temp);
+        temp = chromosome[n].DNA[0].substr(pn1, S1.length());
+        chromosome[n].DNA[0] = replaceStr(chromosome[n].DNA[0], pn1, S1.length(), chromosome[m].DNA[0], pm1, S2.length());
+        chromosome[m].DNA[0] = replaceStr(chromosome[m].DNA[0], pm1, S2.length(), temp);
+        return;
     }
 }
 void cell::reverseMutation(string S1, int n)
 {
-    if (findSubstr(chromosome[n].DNA[0], S1) < findSubstr(chromosome[n].DNA[1], S1))
+    int pos0 = findSubstr(chromosome[n].DNA[0], S1);
+    int pos1 = findSubstr(chromosome[n].DNA[1], S1);
+    if ((pos0 < pos1 or pos1 == -1) and pos0 != -1)
     {
-        int p = findSubstr(chromosome[n].DNA[0], S1);
+        int p = pos0;
         chromosome[n].DNA[0] = reverseStr(chromosome[n].DNA[0], p, p + S1.length());
         chromosome[n].DNA[1] = reverseStr(chromosome[n].DNA[1], p, p + S1.length());
     }
     else
     {
-        int p = findSubstr(chromosome[n].DNA[1], S1);
+        int p = pos1;
         chromosome[n].DNA[0] = reverseStr(chromosome[n].DNA[0], p, p + S1.length());
         chromosome[n].DNA[1] = reverseStr(chromosome[n].DNA[1], p, p + S1.length());
     }
 }
-void cell::palindrome()
+void cell::palindrome(int n)
 {
-    for (int i = 2; i <= DNA[0].length() / 2; i++)
+    bool exist = false;
+    cout << "your palindromes are:\n";
+    for (int i = 2; i <= chromosome[n].DNA[0].length() / 2; i++)
     {
-        for (int j = 0; j < DNA[0].length() - 2 * i; j++)
+        for (int j = 0; j < chromosome[n].DNA[0].length() - 2 * i; j++)
         {
-            string temp = DNA[0].substr(j, i);
+            string temp = chromosome[n].DNA[0].substr(j, i);
             reverse(temp.begin(), temp.end());
-            if (complementary(temp) == DNA[0].substr(j + i, i))
+            if (complementary(temp) == chromosome[n].DNA[0].substr(j + i, i))
             {
-                cout << DNA[0].substr(j, 2 * i) << endl;
+                cout << chromosome[n].DNA[0].substr(j, 2 * i) << endl;
+                exist = true;
             }
         }
+    }
+    if (!exist)
+    {
+        cout << "Your DNA didn't have any palindrome\n";
     }
 }
 void cell::death()
@@ -325,6 +360,8 @@ void cell::death()
         if (n >= 5 or at >= 3 * cg)
         {
             // del
+            cout << "Your cell died\n";
+            return;
         }
     }
 }
